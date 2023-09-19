01e0ea08291f967da86bc4af08959d3072d0ceac
AmgaadShokrallah <amgaad@example.com>
2023-09-19 03:14:58 -0700

    printf

--- main.h
+++ main.h
@@ -8,33 +8,7 @@
 
 #define BUFF_SIZE 1024
 
-/**
- * struct flag - sturct.
- * @Plus: parameter1
- * @Space: parameter2
- * @Hash: parameter3
- */
 
-typedef struct flag
-{
-	int plus;
-	int space;
-	int hash;
-} flag_t;
-
-/**
- * struct str_ct - struct
- * @c: parameter1
- * @f: parameter2
- */
-
-typedef struct str_ct
-{
-	char *c;
-	int (*f)(va_list list, flag_t *f);
-} str_ct;
-
-/* print */
 int _printf(const char *format, ...);
 int print_rot13(char *rot);
 int print_reverse(va_list r);
