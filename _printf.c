3a83c66e2235a029d069a9e7574e4c6f1e25abc8
Mahmoud shaaban <56820354+mahmoudzin@users.noreply.github.com>
2023-09-19 23:49:12 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -25,7 +25,7 @@ int _printf(const char *format, ...)
 			format++;
 			if (*format == '%')
 			{
-				count += (write(1, format, 1));
+				count += (write(1, &format, 1));
 			}
 			else if (*format == 'c')
 			{
@@ -38,7 +38,7 @@ int _printf(const char *format, ...)
 				char *str = va_arg(list, char *);
 				int str_len = strlen(str);
 
-				count += (write(1, str, str_len));
+				count += (write(1, &str, str_len));
 			}
 		}
 		format++;
